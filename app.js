41e943a74752267a4448056b1cdaef1af59faf05
ApolloSolo <matt1teixeira@gmail.com>
2022-03-30 18:35:44 -0400

    find note by id

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 const { notes } = require("./data/notes.json");
 const uniqid = require('uniqid');
 const path = require('path');
-const { createNewNote } = require("./lib/notes");
+const { createNewNote, findById } = require("./lib/notes");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,6 +13,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/api/notes', (req, res) => {
     res.send(notes);
 });
+
+app.get('/api/notes/:id', (req, res) => {
+    const { id } = req.params;
+    const result = findById(id, notes);
+    res.send(result);
+})
 
 app.post('/api/notes', (req, res) => {
     req.body.id = uniqid();
